d673055ce6b4079aac4b3f03ad10c5dfaaeac3ab
Greg Bergé <greg@smooth-code.com>
2020-01-15 16:33:42 +0100

    feat: add id in readme

--- src/components/Readme.js
+++ src/components/Readme.js
@@ -2,16 +2,14 @@ import React from 'react'
 import styled from '@xstyled/styled-components'
 import Markdown from 'react-markdown'
 import remarkEmoji from 'remark-emoji'
+import remarkSlug from 'remark-slug'
+import GithubSlugger from 'github-slugger'
 import { Card, CardBody } from './Card'
 import { Code } from './Code'
 
 const StyledMarkdown = styled(Markdown)`
   color: readme-text;
   line-height: 1.6;
-
-  h1 {
-    display: none;
-  }
 
   h1,
   h2,
@@ -34,14 +32,29 @@ const InlineCode = styled.code`
   line-height: 1.4;
 `
 
-const renderers = {
-  code: ({ language, value }) => <Code language={language}>{value}</Code>,
-  inlineCode: ({ children }) => <InlineCode>{children}</InlineCode>,
+function linkTarget(url) {
+  if (url.startsWith('#')) return null
+  return '_blank'
 }
 
-const plugins = [remarkEmoji]
+const plugins = [remarkEmoji, remarkSlug]
 
 export function Readme({ template }) {
+  const slugger = new GithubSlugger()
+  const renderers = {
+    code: ({ language, value }) => <Code language={language}>{value}</Code>,
+    inlineCode: ({ children }) => <InlineCode>{children}</InlineCode>,
+    heading: props => {
+      if (props.level === 1) return null
+      const text = props.children
+        .map(child => child.props.children)
+        .filter(x => typeof x === 'string')
+        .join('')
+      const id = slugger.slug(text)
+      const Tag = `h${props.level}`
+      return <Tag id={id}>{props.children}</Tag>
+    },
+  }
   function transformImageUri(uri) {
     if (uri.startsWith(template.repoUrl)) {
       return uri
@@ -61,6 +74,7 @@ export function Readme({ template }) {
       <CardBody>
         <StyledMarkdown
           skipHtml
+          linkTarget={linkTarget}
           renderers={renderers}
           transformImageUri={transformImageUri}
           plugins={plugins}
